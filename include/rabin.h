76394c782ff38a2dd06cdbda7e9282c0464b0c6b
Jonathan Stott <jonathan.stott@gmail.com>
2013-03-15 17:46:24 +0000

    Basic working rabin hash implementation
    This is built around a 'rabin fingerprinter' structure instead of the
    global arrays which are more common in many implementations seen.  It is
    also use agnostic, and relies on a callback to do anything with the
    chunks.

---
+++ include/rabin.h
@@ -0,0 +1,59 @@
+/*
+ *
+ */
+
+#ifndef _RABIN_H
+#define _RABIN_H
+
+// taken from pcompress implementation
+#define RAB_POLYNOMIAL_CONST 153191
+#define RAB_WINDOW_SIZE      48
+#define RAB_AVG_BLOCKSIZE    65535
+
+
+struct rabin_node_t {
+    unsigned long long value;
+    struct rabin_node_t *next;
+};
+
+typedef struct {
+    unsigned long long prime;
+    unsigned long      window_size;
+    unsigned long long blk_sz;
+    unsigned long long fingerprint;
+    unsigned long      min_blk_sz;
+    unsigned long      avg_blk_sz;
+    unsigned long      max_blk_sz;
+    unsigned long long polynomial_map[256];
+    struct rabin_node_t cycle[RAB_WINDOW_SIZE];
+    struct rabin_node_t* curr;
+    unsigned char      *buffer;
+    unsigned char      *input_buffer;
+
+} rabin_fingerprinter_t;
+
+// define FP callback
+typedef void (*rfp_chunk_callback)(unsigned long long blk_sz,
+                                   unsigned char *chunk,
+                                   void *userdata);
+
+// initialize a new rabin fingerprinter, ready for use.
+rabin_fingerprinter_t* rabin_fingerprinter_init(unsigned long min_block_size,
+                                                unsigned long max_block_size);
+
+// reset a fingerprinter to allow it to be used on a new file
+void rabin_fingerprinter_reset(rabin_fingerprinter_t *fp);
+
+// free all memoery associated with a fingerprinter
+void rabin_fingerprinter_free(rabin_fingerprinter_t * fingerprinter);
+
+// fingerprint a given file
+int fingerprint_file(rabin_fingerprinter_t * fingerprinter,
+                     FILE *file,
+                     rfp_chunk_callback cb,
+                     void *userdata);
+
+
+
+#endif /* RABIN_H */
+
---
+++ src/rabin.c
@@ -0,0 +1,158 @@
+/*
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "rabin.h"
+
+static void pregenerate_polys(rabin_fingerprinter_t *fp) {
+    // Calculates result = prime ^ windowsize
+    // After that multiplies all 256 bytes with result
+    unsigned long long result = 1;
+    int i;
+    for (i=0; i<fp->window_size; i++)
+        result *= fp->prime;
+
+    for (i=0; i<=256; i++)
+        fp->polynomial_map[i] = i * result;
+
+    return;
+}
+
+static void create_rabin_ring(rabin_fingerprinter_t *fp) {
+    // initializes all the values to 0 and sets things up for cycling of the
+    // hash window
+    int i = 0;
+    for (i = 0; i < (fp->window_size-1); i++) {
+        (fp->cycle+i)->next = (fp->cycle+i+1);
+        (fp->cycle+i)->value = 0;
+    }
+    // close ring
+    (fp->cycle+fp->window_size-1)->next = fp->cycle;
+    
+
+}
+
+void rabin_fingerprinter_reset(rabin_fingerprinter_t *fp) {
+    // reset the ring
+    create_rabin_ring(fp);
+    fp->curr         = fp->cycle;
+
+    // set blocksize and fingerprint to 0
+    fp->blk_sz       = 0;
+    fp->fingerprint  = 0;
+
+}
+
+rabin_fingerprinter_t* rabin_fingerprinter_init(unsigned long min_block_size,
+                                                unsigned long max_block_size) {
+
+    // basic sanity check.
+    if (min_block_size > max_block_size) {
+        return 0;
+    }
+
+    rabin_fingerprinter_t * fp = malloc(sizeof(rabin_fingerprinter_t));
+    if (!fp) {
+        perror("rabin_fingerprinter_init");
+        return 0;
+    }
+
+    fp->prime       = RAB_POLYNOMIAL_CONST;
+    fp->min_blk_sz  = min_block_size;
+    fp->avg_blk_sz  = RAB_AVG_BLOCKSIZE;
+    fp->max_blk_sz  = max_block_size;
+    fp->window_size = RAB_WINDOW_SIZE;
+
+    // pre-generate the rabin polynomials
+    pregenerate_polys(fp);
+
+    // set up the ring buffer
+    create_rabin_ring(fp);
+
+    // set up the buffers
+    fp->buffer       = malloc(fp->max_blk_sz);
+    if (!fp->buffer) {
+        rabin_fingerprinter_free(fp);
+        perror("rabin_fingerprinter_init");
+        return 0;
+    }
+    fp->input_buffer = malloc(fp->min_blk_sz);
+    if (!fp->input_buffer) {
+        rabin_fingerprinter_free(fp);
+        perror("rabin_fingerprinter_init");
+        return 0;
+    }
+
+    // reset to finish setup
+    rabin_fingerprinter_reset(fp);
+
+    return fp;
+}
+
+void rabin_fingerprinter_free(rabin_fingerprinter_t * fp) {
+    if (fp) {
+        if (fp->buffer) {
+            free(fp->buffer);
+            fp->buffer = NULL;
+        }
+        if (fp->input_buffer) {
+            free(fp->input_buffer);
+            fp->input_buffer = NULL;
+        }
+        free(fp);
+        fp = NULL;
+    }
+}
+
+int fingerprint_file(rabin_fingerprinter_t * fp,
+                     FILE *file,
+                     rfp_chunk_callback cb,
+                     void *userdata) {
+    int ch; 
+    int i = 0, read = 0;
+
+    // if there's no file or fingerprinter, give up
+    // likewise, if there's no callback there isn't much point ...
+    if ((!fp)||(!file)||(!cb))
+        return 1;
+
+    // read the file in slabs of the minimum block size into the input buffer
+    while (read = fread(fp->input_buffer, 1, fp->min_blk_sz, file)) {
+
+        // loop over the input buffer
+        for (i = 0; i < read; i++) {
+            // read off the buffer
+            ch = fp->input_buffer[i];
+
+            // copy to the output buffer
+            fp->buffer[fp->blk_sz] = ch;
+
+            // rabin stuff
+            fp->fingerprint *= fp->prime;
+            fp->fingerprint += ch;
+            fp->fingerprint -= fp->polynomial_map[fp->curr->value];
+
+            fp->curr->value = ch;
+            fp->curr = fp->curr->next;
+
+            fp->blk_sz++;
+            // if we have a block we can consider outputting ...
+            if ((fp->blk_sz > fp->min_blk_sz)) {
+
+                // ... check for it having a valid fingerprint or being too large 
+                if (((fp->fingerprint & fp->avg_blk_sz) == 1) || (fp->blk_sz >= fp->max_blk_sz)) {
+                    // call the callback
+                    (*cb)(fp->blk_sz, fp->buffer, userdata);
+                    // reset the block size
+                    fp->blk_sz = 0;
+                }
+            }
+        }
+    }
+    // if we have any spare bytes
+    if (fp->blk_sz != 0) // send them to the callback, too!
+        (*cb)(fp->blk_sz, fp->buffer, userdata);
+    return 0;
+}
