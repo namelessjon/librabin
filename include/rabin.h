3312ecf2416d57ed126efa061dfd06c72e0f5c57
Jon <jonathan.stott@gmail.com>
2013-06-15 18:18:03 +0100

    Rename fingerprinter to splitter
    As that's what this does.  Also, prefix the split_file command with the
    library namespace, to be a good citizen.

--- include/rabin.h
+++ include/rabin.h
@@ -2,6 +2,7 @@
 #define _RABIN_H
 
 // taken from pcompress implementation
+// https://github.com/moinakg/pcompress
 #define RAB_POLYNOMIAL_CONST 153191
 #define RAB_WINDOW_SIZE      48
 #define RAB_AVG_BLOCKSIZE    65535
@@ -25,27 +26,27 @@ typedef struct {
     struct rabin_node_t* curr;
     unsigned char      *buffer;
 
-} rabin_fingerprinter_t;
+} rabin_splitter_t;
 
-// define FP callback
-typedef void (*rfp_chunk_callback)(unsigned long long blk_sz,
+// define chunk handling callback type
+typedef void (*rsp_chunk_callback)(unsigned long long blk_sz,
                                    unsigned char *chunk,
                                    void *userdata);
 
-// initialize a new rabin fingerprinter, ready for use.
-rabin_fingerprinter_t* rabin_fingerprinter_init(unsigned long min_block_size,
+// initialize a new rabin splitter, ready for use.
+rabin_splitter_t* rabin_splitter_init(unsigned long min_block_size,
                                                 unsigned long max_block_size);
 
-// reset a fingerprinter to allow it to be used on a new file
-void rabin_fingerprinter_reset(rabin_fingerprinter_t *fp);
+// reset a splitter to allow it to be used on a new file
+void rabin_splitter_reset(rabin_splitter_t *splitter);
 
-// free all memoery associated with a fingerprinter
-void rabin_fingerprinter_free(rabin_fingerprinter_t * fingerprinter);
+// free all memory associated with a splitter
+void rabin_splitter_free(rabin_splitter_t * splitter);
 
 // fingerprint a given file
-int fingerprint_file(rabin_fingerprinter_t * fingerprinter,
+int rabin_split_file(rabin_splitter_t * splitter,
                      int fileno,
-                     rfp_chunk_callback cb,
+                     rsp_chunk_callback cb,
                      void *userdata);
 
 
--- src/rabin.c
+++ src/rabin.c
@@ -4,45 +4,45 @@
 
 #define _READ_SIZE (32768)
 
-static void pregenerate_polys(rabin_fingerprinter_t *fp) {
+static void pregenerate_polys(rabin_splitter_t *rsp) {
     // Calculates result = prime ^ windowsize
     // After that multiplies all 256 bytes with result
     unsigned long long result = 1;
     unsigned int i;
-    for (i=0; i<fp->window_size; i++)
-        result *= fp->prime;
+    for (i=0; i<rsp->window_size; i++)
+        result *= rsp->prime;
 
     for (i=0; i<=256; i++)
-        fp->polynomial_map[i] = i * result;
+        rsp->polynomial_map[i] = i * result;
 
     return;
 }
 
-static void create_rabin_ring(rabin_fingerprinter_t *fp) {
+static void create_rabin_ring(rabin_splitter_t *rsp) {
     // initializes all the values to 0 and sets things up for cycling of the
     // hash window
     unsigned int i = 0;
-    for (i = 0; i < (fp->window_size-1); i++) {
-        (fp->cycle+i)->next = (fp->cycle+i+1);
-        (fp->cycle+i)->value = 0;
+    for (i = 0; i < (rsp->window_size-1); i++) {
+        (rsp->cycle+i)->next = (rsp->cycle+i+1);
+        (rsp->cycle+i)->value = 0;
     }
     // close ring
-    (fp->cycle+fp->window_size-1)->next  = fp->cycle;
-    (fp->cycle+fp->window_size-1)->value = 0;
+    (rsp->cycle+rsp->window_size-1)->next  = rsp->cycle;
+    (rsp->cycle+rsp->window_size-1)->value = 0;
 }
 
-void rabin_fingerprinter_reset(rabin_fingerprinter_t *fp) {
+void rabin_splitter_reset(rabin_splitter_t *rsp) {
     // reset the ring
-    create_rabin_ring(fp);
-    fp->curr         = fp->cycle;
+    create_rabin_ring(rsp);
+    rsp->curr         = rsp->cycle;
 
     // set blocksize and fingerprint to 0
-    fp->blk_sz       = 0;
-    fp->fingerprint  = 0;
+    rsp->blk_sz       = 0;
+    rsp->fingerprint  = 0;
 
 }
 
-rabin_fingerprinter_t* rabin_fingerprinter_init(unsigned long min_block_size,
+rabin_splitter_t* rabin_splitter_init(unsigned long min_block_size,
                                                 unsigned long max_block_size) {
 
     // basic sanity check.
@@ -50,59 +50,59 @@ rabin_fingerprinter_t* rabin_fingerprinter_init(unsigned long min_block_size,
         return 0;
     }
 
-    rabin_fingerprinter_t * fp = malloc(sizeof(rabin_fingerprinter_t));
-    if (!fp) {
+    rabin_splitter_t * rsp = malloc(sizeof(rabin_splitter_t));
+    if (!rsp) {
         return 0;
     }
 
-    fp->prime       = RAB_POLYNOMIAL_CONST;
-    fp->min_blk_sz  = min_block_size;
-    fp->avg_blk_sz  = RAB_AVG_BLOCKSIZE;
-    fp->max_blk_sz  = max_block_size;
-    fp->window_size = RAB_WINDOW_SIZE;
+    rsp->prime       = RAB_POLYNOMIAL_CONST;
+    rsp->min_blk_sz  = min_block_size;
+    rsp->avg_blk_sz  = RAB_AVG_BLOCKSIZE;
+    rsp->max_blk_sz  = max_block_size;
+    rsp->window_size = RAB_WINDOW_SIZE;
 
     // pre-generate the rabin polynomials
-    pregenerate_polys(fp);
+    pregenerate_polys(rsp);
 
     // set up the ring buffer
-    create_rabin_ring(fp);
+    create_rabin_ring(rsp);
 
     // set up the buffers
-    fp->buffer       = malloc(fp->max_blk_sz);
-    if (!fp->buffer) {
-        rabin_fingerprinter_free(fp);
+    rsp->buffer       = malloc(rsp->max_blk_sz);
+    if (!rsp->buffer) {
+        rabin_splitter_free(rsp);
         return 0;
     }
 
     // reset to finish setup
-    rabin_fingerprinter_reset(fp);
+    rabin_splitter_reset(rsp);
 
-    return fp;
+    return rsp;
 }
 
-void rabin_fingerprinter_free(rabin_fingerprinter_t * fp) {
-    if (fp) {
-        if (fp->buffer) {
-            free(fp->buffer);
-            fp->buffer = NULL;
+void rabin_splitter_free(rabin_splitter_t * splitter) {
+    if (splitter) {
+        if (splitter->buffer) {
+            free(splitter->buffer);
+            splitter->buffer = NULL;
         }
-        free(fp);
-        fp = NULL;
+        free(splitter);
+        splitter = NULL;
     }
 }
 
-int fingerprint_file(rabin_fingerprinter_t * fp,
+int rabin_split_file(rabin_splitter_t * rsp,
                      int fileno,
-                     rfp_chunk_callback cb,
+                     rsp_chunk_callback cb,
                      void *userdata) {
     int ch;
     int i = 0, nread = 0;
     unsigned char input_buffer[_READ_SIZE];
 
-    // if there's no file or fingerprinter, give up
+    // if there's no file or splitter, give up
     // likewise, if there's no callback there isn't much point ...
     // TODO: check file handle is valid
-    if ((!fp)||(!cb))
+    if ((!rsp)||(!cb))
         return 1;
 
     // read the file in slabs of the minimum block size into the input buffer
@@ -114,32 +114,32 @@ int fingerprint_file(rabin_fingerprinter_t * fp,
             ch = input_buffer[i];
 
             // copy to the output buffer
-            fp->buffer[fp->blk_sz] = ch;
+            rsp->buffer[rsp->blk_sz] = ch;
 
             // rabin stuff
-            fp->fingerprint *= fp->prime;
-            fp->fingerprint += ch;
-            fp->fingerprint -= fp->polynomial_map[fp->curr->value];
+            rsp->fingerprint *= rsp->prime;
+            rsp->fingerprint += ch;
+            rsp->fingerprint -= rsp->polynomial_map[rsp->curr->value];
 
-            fp->curr->value = ch;
-            fp->curr = fp->curr->next;
+            rsp->curr->value = ch;
+            rsp->curr = rsp->curr->next;
 
-            fp->blk_sz++;
+            rsp->blk_sz++;
             // if we have a block we can consider outputting ...
-            if ((fp->blk_sz > fp->min_blk_sz)) {
+            if ((rsp->blk_sz > rsp->min_blk_sz)) {
 
                 // ... check for it having a valid fingerprint or being too large 
-                if (((fp->fingerprint & fp->avg_blk_sz) == 1) || (fp->blk_sz >= fp->max_blk_sz)) {
+                if (((rsp->fingerprint & rsp->avg_blk_sz) == 1) || (rsp->blk_sz >= rsp->max_blk_sz)) {
                     // call the callback
-                    (*cb)(fp->blk_sz, fp->buffer, userdata);
+                    (*cb)(rsp->blk_sz, rsp->buffer, userdata);
                     // reset the block size
-                    fp->blk_sz = 0;
+                    rsp->blk_sz = 0;
                 }
             }
         }
     }
     // if we have any spare bytes
-    if (fp->blk_sz != 0) // send them to the callback, too!
-        (*cb)(fp->blk_sz, fp->buffer, userdata);
+    if (rsp->blk_sz != 0) // send them to the callback, too!
+        (*cb)(rsp->blk_sz, rsp->buffer, userdata);
     return 0;
 }
